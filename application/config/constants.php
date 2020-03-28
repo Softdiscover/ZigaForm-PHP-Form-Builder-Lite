06a00b6f7b217a670cfb93994e4c7c26e9ad5595
Raimundo Yabar <raimundo.yabar@incsub.com>
2020-03-28 13:02:44 -0500

    version 4.6.4

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-define('UIFORM_VERSION', '4.6.3');
+define('UIFORM_VERSION', '4.6.4');
 define('ZIGAFORM_F_LITE',1);
 define('UIFORM_DEBUG',0);
 define('UIFORM_DEMO',0);
