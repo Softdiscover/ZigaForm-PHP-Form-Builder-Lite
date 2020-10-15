f39ba070c17210162e70fd28fe99f6afb2c3efc1
Laranginha <info@softdiscover.com>
2020-10-15 03:43:26 -0500

    Version 5.0.9     [fixed] - issue on saving translations

--- application/modules/addon_mgtranslate/assets/backend/js/admin.debug.js
+++ application/modules/addon_mgtranslate/assets/backend/js/admin.debug.js
@@ -149,7 +149,7 @@ if (!$uifm.isFunction(zgfm_back_mgtranslate)) {
 				$.ajax({
 					type        : 'POST', 
 					url: rockfm_vars.uifm_siteurl + 'addon_mgtranslate/zfaddn_mgtranslate_back/ajax_save_pofile',
-					data: $.param(tmpArrs) + '&action=zgfm_mgtranslate_savePo' + '&zgfm_security=' + uiform_vars.ajax_nonce + '&lang=' +$('#mgtr-input-lang').val() + '&side=' + $('#mgtr-input-side').val(),
+					data: $.param(tmpArrs) + '&action=zgfm_mgtranslate_savePo' + '&page=zgfm_form_builder&zgfm_security=' + uiform_vars.ajax_nonce + '&lang=' +$('#mgtr-input-lang').val() + '&side=' + $('#mgtr-input-side').val(),
 					dataType    : 'json', 
 					encode          : true,
 					beforeSend: function () {
